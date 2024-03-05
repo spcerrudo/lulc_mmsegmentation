ca573eabfc3047e4ef26581d7ce72ba9d1efdc65
spcerrudo <75797282+spcerrudo@users.noreply.github.com>
2024-03-05 16:50:11 +0800

    Update rs_image_inference.py
    removed color_map args

--- demo/rs_image_inference.py
+++ demo/rs_image_inference.py
@@ -31,14 +31,6 @@ def main():
         type=int,
         nargs=2)
     parser.add_argument(
-        '--color_map',
-        help='color mapping of segment classes',
-        default=[
-    (0, 0, 0), (128, 0, 0),  (0, 255, 36), (148, 148, 148),  (255, 255, 255),
-    (34, 97, 38),  (0, 69, 255), (75, 181, 73),  (222, 31, 7)
-],
-        type=list)
-    parser.add_argument(
         '--thread', default=1, type=int, help='number of inference threads')
     parser.add_argument(
         '--device', default='cuda:0', help='Device used for inference')
@@ -51,7 +43,7 @@ def main():
         device=args.device)
     image = RSImage(args.image)
 
-    inferencer.run(image, args.window_size, args.stride, args.output_path, args.color_map)
+    inferencer.run(image, args.window_size, args.stride, args.output_path)
 
 
 if __name__ == '__main__':
